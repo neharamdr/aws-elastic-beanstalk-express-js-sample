eebf47c069f3e9c0c536b726a48412301e7c3475
Neha Ram <neha.ram.dr@gmail.com>
2024-08-18 19:34:35 -0400

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('AWS Lab Test by Neha - 18 August 2024!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
